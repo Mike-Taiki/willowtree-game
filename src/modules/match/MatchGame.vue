5c7cab2ab3ed931da413d104bc24fde5690253e1
Michael Gomes <michael@bamboodcm.com>
2024-10-10 06:55:35 -0300

    feat: section photos

--- src/modules/match/MatchGame.vue
+++ src/modules/match/MatchGame.vue
@@ -1,9 +1,19 @@
 <template>
-  <Header></Header>
-  <main class="game">
-    <p class="game__info">Try matching the WillowTree employee to their photo.</p>
-    <h2 class="game__heading">Erika Cober</h2>
-  </main>
+  <div class="game">
+    <Header></Header>
+    <main class="game-content">
+      <p class="game-content__info">Try matching the WillowTree employee to their photo.</p>
+      <h2 class="game-content__heading">Erika Cober</h2>
+      <section class="game-content-photos">
+        <div class="game-content-photos__item"></div>
+        <div class="game-content-photos__item"></div>
+        <div class="game-content-photos__item"></div>
+        <div class="game-content-photos__item"></div>
+        <div class="game-content-photos__item"></div>
+        <div class="game-content-photos__item"></div>
+      </section>
+    </main>
+  </div>
 </template>
 <script lang="ts" setup>
 import Header from '@/shared/components/HeaderComponent.vue'
