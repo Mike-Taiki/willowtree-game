8338555d723c3a8ccfd42080159d80141eeebb2d
Michael Gomes <michael@bamboodcm.com>
2024-10-10 07:02:06 -0300

    refactor: improve type

--- src/modules/match/services/index.ts
+++ src/modules/match/services/index.ts
@@ -2,6 +2,6 @@ import { http } from '@/shared/http'
 import type { Profile } from '../../match/types'
 import type { AxiosResponse } from 'axios'
 
-export async function getProfiles(): Promise<AxiosResponse<Profile>> {
+export async function getProfiles(): Promise<AxiosResponse<Profile[]>> {
   return http.get('/profiles')
 }
