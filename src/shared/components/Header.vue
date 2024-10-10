255eef7ad54cd093f91bc3444892d6420f20b9a6
Michael Gomes <michael@bamboodcm.com>
2024-10-09 22:10:28 -0300

    feat: adjust header

--- src/shared/components/Header.vue
+++ src/shared/components/Header.vue
@@ -1,10 +1,20 @@
 <template>
   <header class="header">
     <nav class="header__nav">
-      <img class="header__title" src="../../assets/Title.svg" alt="" />
+      <i class="header__nav-icon" @click="goBack"></i>
+      <div class="header__nav-title">
+        <img class="header__nav-img" src="../../assets/Title.svg" alt="" />
+      </div>
     </nav>
   </header>
 </template>
+<script lang="ts" setup>
+import router from '@/router'
+
+function goBack() {
+  router.push({ name: 'home' })
+}
+</script>
 <style scoped lang="scss">
 @import './Header.scss';
 </style>
