4f4ff5ae036b6c50e51cc8473d86f6e91fb71dca
Michael Gomes <michael@bamboodcm.com>
2024-10-10 05:59:56 -0300

    refactor: improve button

--- src/modules/home/pages/Home.vue
+++ src/modules/home/pages/Home.vue
@@ -1,43 +1,14 @@
 <template>
-  <main class="wrapper">
-    <img class="wrapper__image" src="../../../assets/Logo.svg" alt="The Name Game" />
-    <p class="wrapper__info">Try matching the WillowTree employee to their photo.</p>
-    <RouterLink class="wrapper__button" to="/match">Play!</RouterLink>
-  </main>
+  <section class="home">
+    <main class="home-content">
+      <img class="home-content__image" src="../../../assets/Logo.svg" alt="The Name Game" />
+      <p class="home-content__info">Try matching the WillowTree employee to their photo.</p>
+      <RouterLink class="home-content__button" to="/match">Play!</RouterLink>
+    </main>
+  </section>
 </template>
 <script setup lang="ts"></script>
 
 <style lang="scss" scoped>
-.wrapper {
-  background-color: $bg-dark-blue;
-  color: white;
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
-  align-items: center;
-  flex: 1;
-  height: 100vh;
-  gap: 20px;
-
-  &__image {
-    max-width: 100%;
-  }
-
-  &__info {
-    font-family: Helvetica, sans-serif;
-    font-weight: 400;
-    font-size: 24px;
-  }
-
-  &__button {
-    background-color: $bg-light-blue;
-    border: none;
-    color: $bg-white;
-    border-radius: 14px;
-    font-size: 17px;
-    font-weight: 700;
-    padding: 12px 100px;
-    text-decoration: none;
-  }
-}
+@import './Home.scss';
 </style>
--- src/shared/components/Header.vue
+++ src/shared/components/Header.vue
@@ -3,7 +3,7 @@
     <nav class="header__nav">
       <i class="header__nav-icon" @click="goBack"></i>
       <div class="header__nav-title">
-        <img class="header__nav-img" src="../../assets/Title.svg" alt="" />
+        <img class="header__nav-img" src="../../assets/Title.svg" alt="The Name Game" />
       </div>
     </nav>
   </header>
